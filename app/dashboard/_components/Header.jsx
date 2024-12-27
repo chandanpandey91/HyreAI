7e482242e4e01a73dd8b7b5185492c238af3a703
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-12-28 01:54:56 +0530

    updated

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,18 +1,19 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { UserButton } from '@clerk/nextjs';
+import Image from 'next/image'; // Correctly import the Image component
+import React from 'react';
 
 function Header() {
   return (
-    <div className='flex p-4 items-center justify-between'>
-        <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
-        <ul className='flex gap-6'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-        <UserButton/>
+    <div className="flex p-4 items-center justify-between">
+      <Image src="/logo.svg" width={160} height={100} alt="logo" />
+      <ul className="flex gap-6">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <UserButton />
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
