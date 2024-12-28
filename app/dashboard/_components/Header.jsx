3704143a175fc227c9ad035de0fb2cfad5453d9e
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-12-28 15:40:36 +0530

    updated

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,13 +1,19 @@
+"use client"
 import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image'; // Correctly import the Image component
-import React from 'react';
+import { usePathname } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 function Header() {
+  const path=usePathname();
+  useEffect(()=>{
+    console.log(path);
+  },[])
   return (
     <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
       <Image src="/logo.svg" width={160} height={100} alt="logo" />
       <ul className="flex gap-6">
-      <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>Home</li>
+      <li className={`hover:text-red-500 hover:font-bold transition-all cursor-pointer ${path==='/dashboard'&&'text-red-500 font-bold'}`}>Home</li>
         <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>About</li>
         <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>Contact</li>
         <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>Check your level</li>
