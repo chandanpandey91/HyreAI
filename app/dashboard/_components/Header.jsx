1b9cd8b294719022cee480ca2345189a815baea4
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-12-28 15:03:48 +0530

    header updated

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -7,10 +7,10 @@ function Header() {
     <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
       <Image src="/logo.svg" width={160} height={100} alt="logo" />
       <ul className="flex gap-6">
-        <li className='hover:text-primary hover:font-bold transition-all'>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Check your level</li>
+      <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>Home</li>
+        <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>About</li>
+        <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>Contact</li>
+        <li className='hover:text-red-500 hover:font-bold transition-all cursor-pointer'>Check your level</li>
       </ul>
       <UserButton />
     </div>
