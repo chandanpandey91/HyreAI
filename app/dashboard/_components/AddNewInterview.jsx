ff8b589b2fadb409ae97f18b88d577eccf764325
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-12-30 18:19:18 +0530

    updated

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+
+  
 
 function AddNewInterview() {
+    const [openDialog,setOpenDialog]=useState(false)
   return (
+   
     <div>
-        <div className='p-10 border rounded-lg'>
-            <h2 className='font-bold text-lg text-center'>+ Add New</h2>
+        <div className='p-10 border rounded-lg bg-secondary hover:scale-105 
+        hover:shadow-md cursor-pointer transition-all' onClick={()=>setOpenDialog(true)}>
+            <h2 className='text-lg text-center'>+ Add New</h2>
         </div>
+
     </div>
   )
 }
